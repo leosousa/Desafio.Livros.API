47dd1f027b9d2185f472f7a39d83dbd375acd2f0
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2024-10-27 00:05:43 -0300

    feat: adicionando camada de infraestrutura ao projeto. Criação do repositório base e primeiro repositório para a entidade assunto

---
+++ src/Livros.Infraestrutura/Repositorios/RepositorioAssunto.cs
@@ -0,0 +1,12 @@
+﻿using Livros.Dominio.Contratos;
+using Livros.Dominio.Entidades;
+using Livros.Infraestrutura.BancoDados;
+
+namespace Livros.Infraestrutura.Repositorios;
+
+public class RepositorioAssunto : Repositorio<Assunto>, IRepositorioAssunto
+{
+    public RepositorioAssunto(LivroDbContext database) : base(database)
+    {
+    }
+}
