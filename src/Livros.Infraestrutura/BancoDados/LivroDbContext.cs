13185fe4152cb32ba12a4bbd79785c0363599228
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2024-10-27 00:05:43 -0300

    feat: adicionando camada de infraestrutura ao projeto. Criação do repositório base e primeiro repositório para a entidade assunto

---
+++ src/Livros.Infraestrutura/BancoDados/LivroDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Livros.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Livros.Infraestrutura.BancoDados;
+
+public class LivroDbContext : DbContext
+{
+    public DbSet<Assunto> Assuntos { get; set; }
+
+    public LivroDbContext()
+    {
+    }
+
+    public LivroDbContext(DbContextOptions<LivroDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/Livros.Infraestrutura/BancoDados/Mapeamentos/AssuntoConfiguracao.cs
@@ -0,0 +1,20 @@
+﻿using Livros.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Livros.Infraestrutura.BancoDados.Mapeamentos;
+
+public class AssuntoConfiguracao : IEntityTypeConfiguration<Assunto>
+{
+    public void Configure(EntityTypeBuilder<Assunto> builder)
+    {
+        builder.ToTable(nameof(Assunto));
+
+        builder.HasKey(c => c.Id);
+
+        builder
+           .Property(propriedade => propriedade.Descricao)
+           .IsRequired()
+           .HasMaxLength(20);
+    }
+}
