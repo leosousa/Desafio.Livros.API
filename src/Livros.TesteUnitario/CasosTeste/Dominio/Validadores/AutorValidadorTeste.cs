8ad9edc3e7793d4ef259ef78d85437b8144d6dcf
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2024-10-29 10:20:27 -0300

    test: adicionando teste unitário para o validador de autor

---
+++ src/Livros.TesteUnitario/CasosTeste/Dominio/Validadores/AutorValidadorTeste.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using FluentValidation.TestHelper;
+using Livros.Dominio.Entidades;
+using Livros.Dominio.Recursos;
+using Livros.Dominio.Validadores;
+
+namespace Livros.TesteUnitario.CasosTeste.Dominio.Validadores;
+
+public class AutorValidadorTeste
+{
+    private AutorValidador GerarCenario()
+    {
+        return new AutorValidador();
+    }
+
+    [Fact(DisplayName = $"Deve retornar {Mensagens.NomeECampoObrigatorio} quando o nome não for enviado")]
+    public async Task DeveRetornarNaoInformado_QuandoAutorForNulo()
+    {
+        // Arrange
+        var autorEnviado = new Autor(nome: null);
+
+        // Act
+        var resultado = GerarCenario().TestValidate(autorEnviado!);
+
+        // Assert
+        Assert.False(resultado.IsValid);
+        Assert.Contains(resultado.Errors, erro => erro.ErrorMessage == Mensagens.NomeECampoObrigatorio);
+    }
+
+    [Fact(DisplayName = $"Deve retornar mensagem quando o nome for maior que o tamanho máximo permitido")]
+    public async Task DeveRetornarMensagemMaximaCaracteres_QuandoAutorForMaiorQuePermitido()
+    {
+        // Arrange
+        var autorEnviado = new Autor(nome: new Faker().Random.String(Autor.AUTOR_NOME_MAXIMO_CARACTERES + 1));
+
+        // Act
+        var resultado = GerarCenario().TestValidate(autorEnviado!);
+
+        // Assert
+        Assert.False(resultado.IsValid);
+        Assert.Contains(resultado.Errors, erro => erro.ErrorMessage == Mensagens.NomePodeTerAteXCaracteres);
+    }
+}
