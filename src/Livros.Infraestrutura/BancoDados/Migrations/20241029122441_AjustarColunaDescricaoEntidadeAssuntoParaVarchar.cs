31175027afb0fe5a4e425000a5bb6d5274272ea6
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2024-10-29 09:25:58 -0300

    refactor: ajustando coluna da entidade Assunto para varchar

---
+++ src/Livros.Infraestrutura/BancoDados/Migrations/20241029122441_AjustarColunaDescricaoEntidadeAssuntoParaVarchar.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livros.Infraestrutura.BancoDados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustarColunaDescricaoEntidadeAssuntoParaVarchar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Assunto",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Assunto",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
