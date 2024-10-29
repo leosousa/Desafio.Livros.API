e261cc0bb169c50109c7014bcf352f02eac6102b
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2024-10-29 09:46:18 -0300

    feat: Criando o validaddor da entdade Autor.

---
+++ src/Livros.Dominio/Validadores/AutorValidador.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Livros.Dominio.Entidades;
+using Livros.Dominio.Recursos;
+
+namespace Livros.Dominio.Validadores;
+
+public class AutorValidador : AbstractValidator<Autor>
+{
+    public AutorValidador()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+                .WithMessage(Mensagens.NomeECampoObrigatorio)
+            .MaximumLength(20)
+                .WithMessage(Mensagens.NomePodeTerAteXCaracteres);
+    }
+}
