9e6e1d42a2f4ee7bffcf14cb05af1abd72700e44
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2025-01-07 13:59:53 -0300

    feat: adicionada view de relatório de produção literária

---
+++ src/Livros.API/Controllers/RelatorioController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Livros.API.Controllers;
+
+[Route("api/relatorios")]
+public class RelatorioController : ApiControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RelatorioController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("producao-literaria/autor")]
+    public async Task<IActionResult> GerarRelatorioProducaoLiteraria()
+    {
+        return null;
+    }
+}
---
+++ src/Livros.Infraestrutura/BancoDados/Migrations/20250107160224_AddReport.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livros.Infraestrutura.BancoDados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE VIEW [dbo].[ViewRelatorioLivrosPorAutor] AS
+SELECT 
+    A.Nome AS NomeAutor,
+    L.Titulo AS TituloLivro,
+    L.AnoPublicacao,
+    STRING_AGG(ASS.Descricao, ', ') AS Assuntos
+FROM 
+    Autor A
+INNER JOIN 
+    Livro_Autor LA ON A.Id = LA.AutorId
+INNER JOIN 
+    Livro L ON LA.LivroId = L.Id
+LEFT JOIN 
+    Livro_Assunto LAS ON LAS.LivroId = L.Id
+INNER JOIN 
+    Assunto ASS ON LAS.AssuntoId = ASS.Id
+GROUP BY 
+    A.Nome, L.Titulo, L.AnoPublicacao;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+DROP VIEW [dbo].[ViewRelatorioLivrosPorAutor];
+            ");
+        }
+    }
+}
