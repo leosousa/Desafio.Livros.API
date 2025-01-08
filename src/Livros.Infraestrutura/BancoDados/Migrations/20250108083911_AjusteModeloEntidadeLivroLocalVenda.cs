0a4fcac862b02be4654d3eb9341c4222baff4ddc
Leonardo Sousa <leonardo.sousa.ti@gmail.com>
2025-01-08 05:41:00 -0300

    feat: ajustado mapeamento do modelo

---
+++ src/Livros.Infraestrutura/BancoDados/Migrations/20250108083911_AjusteModeloEntidadeLivroLocalVenda.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livros.Infraestrutura.BancoDados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteModeloEntidadeLivroLocalVenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LivroLocalVenda",
+                table: "LivroLocalVenda");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LivroLocalVenda_LivroId",
+                table: "LivroLocalVenda");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LivroLocalVenda",
+                table: "LivroLocalVenda",
+                columns: new[] { "LivroId", "LocalVendaId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LivroLocalVenda",
+                table: "LivroLocalVenda");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LivroLocalVenda",
+                table: "LivroLocalVenda",
+                columns: new[] { "IdLivro", "IdLocalVenda" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LivroLocalVenda_LivroId",
+                table: "LivroLocalVenda",
+                column: "LivroId");
+        }
+    }
+}
